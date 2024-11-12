986fdb69edd429aef43b8aff546ebe0dc1442e87
weslainesantana <weslaine2029@gmail.com>
2024-11-12 15:42:13 -0300

    Feat: feito exercicio13

--- exercicio13/src/main/java/com/weslaine/exercicio13/models/PlayerEntity.java
+++ exercicio13/src/main/java/com/weslaine/exercicio13/models/PlayerEntity.java
@@ -1,11 +1,9 @@
 package com.weslaine.exercicio13.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
-import jakarta.persistence.UniqueConstraint;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
